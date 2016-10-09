e3c787bc63e362499bbefe066eda4cbe331c1575
gonotes <aganno2@gmail.com>
2016-10-09 09:07:35 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,8 +3,8 @@
 export USER=root
 
 cd /opt/contrail
-
-repo init -u https://github.com/Juniper/contrail-vnc -m noauth.xml
+tar -xf repo.tar
+#repo init -u https://github.com/Juniper/contrail-vnc -m noauth.xml
 repo sync
 
 python /opt/contrail/third_party/fetch_packages.py
